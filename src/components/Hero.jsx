00517a9533fa20c28de4821494f571de09175d1f
Ankit kushwaha <119472022+Ankitkushwaha90@users.noreply.github.com>
2023-12-07 16:01:41 -0800

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,11 +16,10 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
+            Hi, I'm <span className='text-[#915EFF]'>Ankit kushwaha.</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+            B_tech 2nd year in Nitra Techenical Campouse<br className='sm:block hidden' />
           </p>
         </div>
       </div>
